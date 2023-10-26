e5737c0efcdb4722f0a367f0e89d947775913b12
pezzinut <sydneyk0521@gmail.com>
2023-10-26 15:53:51 -0700

    lab 5
    Made changes to lab 5!

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - LAB 5: Data and Variables
+// Author: Sydney Kim
+// Date: October 26, 2023
 
-// Constants
+// Define Variables 
+make = "Toyota" ; 
+model = "Camry" ;
+color = "Rusty" ;
+year = 2022;
 
-// Functions
+// calculate
+age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age" + age + "years<br>"); 
